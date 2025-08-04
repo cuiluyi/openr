6cb50dd8d178a66c33ae6f2ac9da0ecb6daf2f0e
cuiluyi <luyicui@qq.com>
2025-08-05 00:45:42 +0800

    test git rm

--- test/test.py
+++
@@ -1,8 +0,0 @@
-from vllm import LLM, SamplingParams
-
-llm = LLM(
-    model="/data/cuiluyi/resources/models/Qwen/Qwen2.5-Math-1.5B-Instruct",
-    trust_remote_code=True,
-    gpu_memory_utilization=0.5,
-)
-print("LLM initialized successfully.")
